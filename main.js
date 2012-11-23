22cb87dcf16d3611680d21d6681e9240a9391fb7
Yus Uf <cakyus@gmail.com>
2012-11-23 13:19:03 +0700

    implement html5 layout

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
 window.addEventListener('load', function() {
+	
 	$('#preview').html(wiky.process($('#textarea').val()));
+	
+	// -- Event Listener --
+	$('input[name="buttonViewWiki"]').click(function(){
+		alert('buttonViewWiki');
+	});
+	$('input[name="buttonViewHTML"]').click(function(){
+		alert('buttonViewHTML');
+	});
 });
